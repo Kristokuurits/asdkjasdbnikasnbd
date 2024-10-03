b73349c17f04f4203b122c5af4ddbc2914b7453c
Opilane TTHK <opilane@tthk.ee>
2024-10-03 11:24:17 +0300

    a

---
+++ WpfApp1/WpfApp1/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Chat.MVVM.Core;
+using Chat.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        private Server _server;
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer());
+        }
+    }
+}
